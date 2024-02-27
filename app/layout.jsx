19a272951df0a2dc9dc37158bd0d2220ad32ca1c
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-28 00:57:28 +0530

    project finalized

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,28 @@
 import "@styles/globals.css";
-import Nav from "@Components/Nav";
+
+import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopedia",
-  description: "Discover & Share AI prompts",
+  description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient"></div>
-          </div>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
