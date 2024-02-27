4c39a148b41df8071a5389caf3b53219c964ab82
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-27 19:43:44 +0530

    form component completed

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const CreatePrompt = async (e) => {};
+  return (
+    <Form
+      type="Create"
+      post={post}
+      submitting={submitting}
+      handleSubmit={CreatePrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
