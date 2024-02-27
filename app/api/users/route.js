80a2a6aaa3533818b9325e4ad89be1805d709dbe
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-27 09:07:28 +0530

    Prompotopia setup initialized

---
+++ app/api/users/route.js
@@ -0,0 +1,12 @@
+// http://localhost:3000/api/users
+
+export async function GET(request) {
+  const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Bob" },
+  ];
+
+  //send the users as a response
+  return new Response(JSON.stringify(users));
+}
---
+++ app/posts/new/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>new posts</div>;
+};
+
+export default page;
