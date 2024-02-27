37dc86f4ce54a97fdcfbc5da25911a5744bcc748
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-27 15:50:27 +0530

    route authentication done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import Profile from "@components/Profile";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 console.log({
@@ -15,15 +16,33 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ Profile }) {
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+  async signIn({ profile }) {
     try {
       //serverless route ,i.e, Lambda function
       await connectToDB();
 
       //check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
 
       //if not, create a user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
