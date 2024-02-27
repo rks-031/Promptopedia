882c1bb08c09e617bec80cd3c967a0a08aa8e1af
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-27 21:01:33 +0530

    prompts intialized in the database

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,21 +1,15 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const POST = async (req) => {
-  const { userId, prompt, tag } = await req.json();
+export const POST = async (request) => {
+  const { userId, prompt, tag } = await request.json();
 
   try {
-    await connectToDB(); //lambda function -> will die after it's job is done
-    const newPrompt = new Prompt({
-      creator: userId,
-      prompt,
-      tag,
-    });
-    await newPrompt.save();
+    await connectToDB();
+    const newPrompt = new Prompt({ creator: userId, prompt, tag });
 
-    return new Response(JSON.stringify(newPrompt), {
-      status: 201,
-    });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {
     return new Response("Failed to create a new prompt", { status: 500 });
   }
