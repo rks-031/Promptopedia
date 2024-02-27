65fa3cc11a67204024222342be1da084f82744a9
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-27 15:26:11 +0530

    user model created

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import Profile from "@components/Profile";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -15,7 +16,20 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ Profile }) {},
+  async signIn({ Profile }) {
+    try {
+      //serverless route ,i.e, Lambda function
+      await connectToDB();
+
+      //check if a user already exists
+
+      //if not, create a user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
